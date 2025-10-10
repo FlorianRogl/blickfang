7f31489da3882f3e0564e091cb0f7e718dc282bb
roglf <rogflc21@htl-kaindorf.at>
2025-10-10 12:27:10 +0200

    host push

--- src/components/Homepage/HeroCarousel.tsx
+++ src/components/Homepage/HeroCarousel.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import picture13 from '../../assets/Picture10.jpg'
 import Kursraum from '../../assets/Kursraum.jpg'
-import picture20 from '../../assets/slide1.jpg'
+import picture20 from '../../assets/Slide1.jpg'
 
 
 interface CarouselImage {
